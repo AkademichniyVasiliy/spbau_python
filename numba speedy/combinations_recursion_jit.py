d4c1301ac2eb2ba20008c5ee88ff1cb8307007bd
AkademichniyVasiliy <34216819+AkademichniyVasiliy@users.noreply.github.com>
2022-05-19 04:10:06 +0300

    Update combinations_recursion_jit.py

--- numba speedy/combinations_recursion_jit.py
+++ numba speedy/combinations_recursion_jit.py
@@ -10,6 +10,7 @@ def combinations (n, k):
         return 1
     return combinations(n-1, k) + combinations(n-1, k-1)
 
+
 @jit(nopython=True)
 def combinations_jit(n, k):
     if n < 0 or k < 0:
@@ -26,3 +27,4 @@ print(f'{n} choose {k} is {combinations_jit(n,k)}, Executed with jit in: {time.t
 start_time = time.time()
 print(f'{n} choose {k} is {combinations(n,k)}, Executed without jit in: {time.time()-start_time} seconds')
 print('Massive difference huh')
+
